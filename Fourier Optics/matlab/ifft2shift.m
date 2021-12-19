73e0eb6d69aa963de57ae26523f94e904bada466
Louis Delait <louis.delait@student.uclouvain.be>
2021-12-19 12:05:17 +0100

    Delete Fourier Optics/matlab directory

--- Fourier Optics/matlab/ifft2shift.m
+++
@@ -1,9 +0,0 @@
-function F = ifft2shift(f)
-% 2D Inverse Fourier Transform. 
-% The quadrants are shifted before and after the FT
-% Then the coordinates origin is taken at the center of the matrix
-%
-% Sintaxis
-%   F = fft2shift(f);
-
-F = fftshift( ifft2 (fftshift (f) ) );
